57ed68f7fa7ec4ffa0177765230fa0c431d16a62
Leonardo Arevalo <larevalo@zeni.com.ar>
2023-01-28 16:22:00 -0300

    initial commit

---
+++ src/main/java/design/pattern/creational/builder/CarBuilder.java
@@ -0,0 +1,14 @@
+package design.pattern.creational.builder;
+
+public interface CarBuilder {
+
+	public void buildBodyStyle();
+	public void buildPower();
+	public void buildEngine();
+	public void buildBreaks();
+	public void buildSeats();
+	public void buildWindows();
+	public void buildFuelType();
+	public Car getCar();
+	
+}
