8b6891603efe6671b008807446003864e6820f96
Leonardo Arevalo <larevalo@zeni.com.ar>
2023-01-28 10:16:08 -0300

    initial commit

---
+++ src/main/java/design/pattern/structural/composite/o1/Componente.java
@@ -0,0 +1,12 @@
+package design.pattern.structural.composite.o1;
+
+public abstract class Componente {
+	protected String nombre;
+	public Componente (String nombre)
+	{
+		this.nombre = nombre;
+	}
+	abstract public void agregar(Componente c);
+	abstract public void eliminar(Componente c);
+	abstract public void mostrar(int profundidad);
+}
---
+++ src/main/java/design/pattern/structural/composite/o1/Hoja.java
@@ -0,0 +1,20 @@
+package design.pattern.structural.composite.o1;
+
+public class Hoja extends Componente{
+	public Hoja (String nombre)
+	{
+		super(nombre);
+	}
+	public void agregar(Componente c)
+	{
+		System.out.println("no se puede agregar la hoja");
+	}
+	public void eliminar(Componente c)
+	{
+		System.out.println("no se puede quitar la hoja");
+	}
+	public void mostrar(int depth)
+	{
+		System.out.println('-' + "" + nombre);
+	}
+}
